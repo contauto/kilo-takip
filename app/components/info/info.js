3a8e4329688895412b2d534992f024e131f1b6f6
Kağan Berke Maktav <controleng@yandex.com>
2023-06-29 11:12:54 +0300

    text color

--- app/components/info/info.js
+++ app/components/info/info.js
@@ -4,7 +4,22 @@ function Info({height, weight, date, bmi}) {
     return (
         <div>
             {height && <div className="mb-5 border border-black p-5 rounded-full text-xl font-semibold mx-8">
-                {date} tarihinde boyunuz {height} cm, kilonuz {weight} kg ve vücut kitle indeksiniz {bmi} olarak
+                <span className="text-white">
+                    {date}&nbsp;
+                </span>
+                 tarihinde boyunuz&nbsp;
+                <span className="text-white">
+                    {height}&nbsp;
+                </span>
+                 cm, kilonuz&nbsp;
+                <span className="text-white">
+                    {weight}&nbsp;
+                </span>
+                 kg ve vücut kitle indeksiniz&nbsp;
+                <span className="text-white">
+                    {bmi}&nbsp;
+                </span>
+                 olarak
                 hesaplanmıştır.
             </div>}
             {!height && <div className="mb-5 border border-black p-5 rounded-full text-xl font-semibold mx-8">
